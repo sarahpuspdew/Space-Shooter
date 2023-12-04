bfa5d667716b1bb6ac2bb0097025650bd2fc944a
Sarah Puspa Dewi <sarahpuspadewi.spd@gmail.com>
2023-12-04 09:40:41 +0700

    Update Background.gd

--- Scripts/Background.gd
+++ Scripts/Background.gd
@@ -6,6 +6,6 @@ extends ParallaxBackground
 
 
 func _process(delta):
-	space_layer.motion_offset += 2 * delta
-	far_stars_layer.motion_offset += 5 * delta
+	space_layer.motion_offset.y += 2 * delta
+	far_stars_layer.motion_offset.y += 5 * delta
 	close_stars_layer.motion_offset.y += 20 * delta
